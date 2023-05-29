f15f546f10147a0d77b1e440a4022fb6262317ba
dabotxt <1095450045@qq.com>
2023-05-29 17:20:38 +0800

    fix: remove Useless file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import { setupRouterGuard } from 'src/router/guard'
 
 const routes: Array<RouteRecordRaw> = [
@@ -21,7 +21,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes
 })
 setupRouterGuard(router)
